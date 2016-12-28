7b2778cf992a462bf15f9ddff986031bc39b53ac
Adhil Roshan N A <adhilr96@gmail.com>
2016-12-28 16:51:52 +0530

    Update Rotation.c

--- Rotation.c
+++ Rotation.c
@@ -4,10 +4,11 @@ int main()
 		int n,s,r;
 		scanf("%d%d",&n,&s);
 		for(i=0;i<s;i++){
-				r=n%10;
-				n=(n/10)+(r*1000);
+				r=n%10000;
+				n=(n/10000)+(r*10);
 			}
 			printf("Rotated integers:%d",n);
   return 0;
 
 }
+
