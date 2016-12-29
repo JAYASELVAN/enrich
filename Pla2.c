e45e43cf28cc1d30f884f1c123e044b11fec2372
Adhil Roshan N A <adhilr96@gmail.com>
2016-12-29 17:02:12 +0530

    Create Pla2.c

---
+++ Pla2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i=0,r,f=1,l;
+    char s[10];
+    scanf("%s",&n);
+    l=strlen(n);
+    for(i=0;i<l/2;i++)
+    {
+        if(n[i]!=n[l-i-1])
+        {
+            f=0;
+            break;
+        }
+    }
+    if(f==1)
+        printf("Yes");
+    else
+        printf(No");
+    return 0;
+}
+    
