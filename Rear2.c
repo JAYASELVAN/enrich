0b2f0bf93981e4af83922967c6da5a43c976010b
Adhil Roshan N A <adhilr96@gmail.com>
2016-12-29 17:42:19 +0530

    Create Rear2.c

---
+++ Rear2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,l=0,j,n;
+    int a[10],r;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        a[l++]=n%10;
+        n=n/10;
+    } 
+    for(i=0;i<l-1;i++)
+    {
+        for(j=i+1;j<l-1;j++)
+        {
+            if(a[i]>a[j])
+            {
+                r=a[i];
+                a[i]=a[j];
+                a[j]=r;
+            }
+        }
+    }
+    for(i=0;i<l-1;i++)
+        printf("%d",a[i]);
+    return 0;
+}
+    
