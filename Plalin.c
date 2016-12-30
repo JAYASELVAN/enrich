6154a3cc34499230846c9935a4e17f217efc72b6
Adhil Roshan N A <adhilr96@gmail.com>
2016-12-30 16:54:39 +0530

    Create Plalin.c

---
+++ Plalin.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string>
+int main()
+{
+    char a[30];
+    int l,i;
+    scanf("%s",&a);
+    l=strlen(a);
+    for(i=0;i<l/2;i++)
+    {
+        if(a[i]!=a[l-1-i])
+        {
+            printf("No");
+            return 0;
+        }
+     
+     }   
+     printf ("Yes");
+     return 0;
+}
