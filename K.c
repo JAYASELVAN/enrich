5e162330afb847138d86f2ffa9d0985d7b73f71a
Adhil Roshan N A <adhilr96@gmail.com>
2016-12-30 17:54:35 +0530

    Update K.c

--- K.c
+++ K.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,k,i,j,a[10][10]={0};
+    int n,m,k,i,j,a[10][10];
     scanf("%d%d%d",n,m,k);
     for(i=0;i<n;i++)
         for(j=0;j<m;j++)
         {
             if(i+j == k)
                 a[i]=1;
+            else 
+                a[i]=0;
             printf("%d ",&a[i]);
         }
     return 0;
